5b869e9d36c61c14657ea2e07732eda57a752473
it63070160 <71114745+it63070160@users.noreply.github.com>
2022-04-30 14:58:35 +0700

    Update blog.js

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -3,20 +3,21 @@ const path = require("path");
 const pool = require("../config");
 const fs = require("fs");
 const multer = require("multer");
-const { isLoggedIn } = require('../middlewares')
+const { isLoggedIn } = require('../middlewares');
+const { date } = require("joi");
 
 router = express.Router();
 
 const blogOwner = async (req, res, next) => {
-  if (req.user.role === 'admin'){
+  if (req.user.role === 'employee'){
     return next()
   }
   
-  const [[blog]] = await pool.query('SELECT * FROM account WHERE user_id=?', [req.params.id])
+  // const [[blog]] = await pool.query('SELECT * FROM account WHERE user_id=?', [req.params.id])
 
-  if (blog.create_by_id !== req.user.id) {
-    return res.status(403).send('You do not have permission to perform this action')
-  }
+  // if (blog.create_by_id !== req.user.id) {
+  //   return res.status(403).send('You do not have permission to perform this action')
+  // }
 
   next()
 }
@@ -65,40 +66,85 @@ router.put("/blogs/addlike/:id", async function (req, res, next) {
   }
 });
 
+// router.post("/blogs", isLoggedIn, upload.array("myImage", 5), async function (req, res, next) {
+
+//   const title = req.body.title;
+//   const content = req.body.content;
+//   const status = req.body.status;
+//   const pinned = req.body.pinned;
+
+//   // Begin transaction
+//   const conn = await pool.getConnection();
+//   await conn.beginTransaction();
+//   console.log(title)
+//   try {
+//     // let results = await conn.query(
+//     //   "INSERT INTO blogs(title, content, status, pinned, `like`, create_date, create_by_id) " +
+//     //   "VALUES(?, ?, ?, ?, 0, CURRENT_TIMESTAMP, ?);",
+//     //   [title, content, status, pinned, req.user.id]
+//     // );
+//     // const blogId = results[0].insertId;
+
+//     // req.files.forEach((file, index) => {
+//     //   let path = [blogId, file.path.substring(6), index == 0 ? 1 : 0];
+//     //   pathArray.push(path);
+//     // });
+
+//     // await conn.query(
+//     //   "INSERT INTO images(blog_id, file_path, main) VALUES ?;",
+//     //   [pathArray]
+//     // );
+
+//     await conn.commit();
+//     res.send("success!");
+//   } catch (err) {
+//     await conn.rollback();
+//     return res.status(400).json(err);
+//   } finally {
+//     conn.release();
+//   }
+// });
+
 router.post("/blogs", isLoggedIn, upload.array("myImage", 5), async function (req, res, next) {
-  const file = req.files;
-  let pathArray = [];
-
-  if (!file) {
-    return res.status(400).json({ message: "Please upload a file" });
-  }
-
-  const title = req.body.title;
-  const content = req.body.content;
-  const status = req.body.status;
-  const pinned = req.body.pinned;
+  const accountnumber = req.body.accountnumber;
+  const interest = req.body.interest;
+  // const firstname = req.body.firstname;
+  // const lastname = req.body.lastname;
+  // const sex = req.body.sex;
+  // const status = req.body.status;
+  // const age = req.body.age;
+  // const salary = req.body.salary;
+  const cardType = req.body.cardType;
 
   // Begin transaction
   const conn = await pool.getConnection();
   await conn.beginTransaction();
+  var today = new Date();
+  var dates = today.getFullYear()+'-'+(today.getMonth()+1).toString().padStart(2, '0')+'-'+today.getDate().toString().padStart(2, '0');
+  var exp_date = (today.getFullYear()+2)+'-'+(today.getMonth()+1).toString().padStart(2, '0')+'-'+today.getDate().toString().padStart(2, '0');
 
   try {
-    let results = await conn.query(
-      "INSERT INTO blogs(title, content, status, pinned, `like`, create_date, create_by_id) " +
-      "VALUES(?, ?, ?, ?, 0, CURRENT_TIMESTAMP, ?);",
-      [title, content, status, pinned, req.user.id]
-    );
-    const blogId = results[0].insertId;
-
-    req.files.forEach((file, index) => {
-      let path = [blogId, file.path.substring(6), index == 0 ? 1 : 0];
-      pathArray.push(path);
-    });
-
-    await conn.query(
-      "INSERT INTO images(blog_id, file_path, main) VALUES ?;",
-      [pathArray]
-    );
+    if (cardType == 'CREDIT_ACCOUNT'){
+      let maxnumber = await conn.query("SELECT max(card_number) as maxnum FROM credit_card")
+      let lastnumber = (parseInt(maxnumber[0][0].maxnum)+1).toString().padStart(16, '0')
+      await conn.query(
+        'INSERT INTO credit_account(account_number) VALUES(?)',
+        [accountnumber]
+      );
+      await conn.query(
+        'INSERT INTO credit_card(card_number, `limit`, outstanding_balance, `status`, interest, issue_date, exp_date, account_number) VALUES(?, 0, 0, ?, ?, ?, ?, ?)',
+        [lastnumber, "Active", interest, dates, exp_date, accountnumber]
+      );
+    }
+    else{
+      let maxnumber = await conn.query("SELECT max(card_number) as maxnum FROM debit_card")
+      let lastnumber = (parseInt(maxnumber[0][0].maxnum)+1).toString().padStart(16, '0')
+      await conn.query(
+        "INSERT INTO debit_card(card_number, limit_per_day, status, issue_date, exp_date, emp_id, account_number) " +
+        "VALUES(?, 0, ?, ?, ?, ?, ?);",
+        [lastnumber, "Active", dates, exp_date, 1, accountnumber]
+      );
+    }
 
     await conn.commit();
     res.send("success!");
@@ -118,14 +164,12 @@ router.get("/blogs/:id", function (req, res, next) {
   const promise2 = pool.query("SELECT * FROM debit_card WHERE account_number=?", [
     req.params.id,
   ]);
-  
+
   // Use Promise.all() to make sure that all queries are successful
   Promise.all([promise1, promise2])
     .then((results) => {
       const [creditCard, blogFields] = results[0];
       const [debitCard, commentFields] = results[1];
-      console.log(creditCard)
-      console.log(debitCard)
       res.json({
         creditCard: creditCard,
         debitCard: debitCard,
@@ -136,41 +180,31 @@ router.get("/blogs/:id", function (req, res, next) {
     });
 });
 
-router.put("/blogs/:id", isLoggedIn, blogOwner, upload.array("myImage", 5), async function (req, res, next) {
+router.put("/card/:id", isLoggedIn, blogOwner, upload.array("myImage", 5), async function (req, res, next) {
   // Your code here
-  const file = req.files;
-  let pathArray = []
-
-  if (!file) {
-    const error = new Error("Please upload a file");
-    error.httpStatusCode = 400;
-    next(error);
-  }
-
-  const title = req.body.title;
-  const content = req.body.content;
-  const status = req.body.status;
-  const pinned = req.body.pinned;
+  const cardLimit = req.body.cardLimit
+  const cardIssue = req.body.cardIssue
+  const cardExp = req.body.cardExp
+  const cardStatus = req.body.cardStatus
+  const cardType = req.body.cardType
 
   const conn = await pool.getConnection()
   await conn.beginTransaction();
 
   try {
-    console.log(content)
-    let results = await conn.query(
-      "UPDATE blogs SET title=?, content=?, status=?, pinned=? WHERE id=?",
-      [title, content, status, pinned, req.params.id]
-    )
-
-    if (file.length > 0) {
-      file.forEach((file, index) => {
-        let path = [req.params.id, file.path.substring(6), 0]
-        pathArray.push(path)
-      })
-
+    if(cardType=='CREDIT_ACCOUNT'){
+      const cardOSB = req.body.cardOSB
+      const cardInterest = req.body.cardInterest
       await conn.query(
-        "INSERT INTO images(blog_id, file_path, main) VALUES ?;",
-        [pathArray])
+        "UPDATE credit_card SET `limit`=?, outstanding_balance=?, `status`=?, interest=?, issue_date=?, exp_date=? WHERE account_number = ?",
+        [cardLimit, cardOSB, cardStatus, cardInterest, cardIssue, cardExp, req.params.id]
+      )
+    }
+    else{
+      await conn.query(
+        "UPDATE debit_card SET limit_per_day=?, status=?, issue_date=?, exp_date=? WHERE account_number = ?",
+        [cardLimit, cardStatus, cardIssue, cardExp, req.params.id]
+      )
     }
 
     await conn.commit()
