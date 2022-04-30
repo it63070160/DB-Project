de49c5fc6d8ab80ad59bd8c34cc37a8fa106040d
OHMpwpp <71824235+ohmzasa203@users.noreply.github.com>
2022-04-30 17:19:13 +0700

    Add files via upload

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,7 +7,7 @@ router = express.Router();
 router.get("/", isLoggedIn, async function (req, res, next) {
   try {
     // const search = req.query.search || ''
-    let sql = "SELECT a.* FROM account as a JOIN (SELECT * FROM credit_account) as b ON a.account_number = b.account_number WHERE user_id=? UNION SELECT c.* FROM account as c JOIN (SELECT * FROM deposit_account) as d ON c.account_number = d.account_number WHERE user_id=?"
+    let sql = "SELECT * FROM account as a JOIN (SELECT * FROM credit_account) as b ON a.account_number = b.account_number JOIN (SELECT * FROM employee) as emp ON a.emp_id = emp.emp_id JOIN (SELECT * FROM branch) as br ON emp.branch_id = br.branch_id JOIN (SELECT * FROM bank) as ba ON br.bank_id = ba.bank_id WHERE user_id=? UNION SELECT * FROM account as c JOIN (SELECT * FROM deposit_account) as d ON c.account_number = d.account_number JOIN (SELECT * FROM employee) as emp ON c.emp_id = emp.emp_id JOIN (SELECT * FROM branch) as br ON emp.branch_id = br.branch_id JOIN (SELECT * FROM bank) as ba ON br.bank_id = ba.bank_id WHERE user_id=?;"
     // let cond = []
 
     // if (search.length > 0) {
@@ -20,6 +20,7 @@ router.get("/", isLoggedIn, async function (req, res, next) {
     console.log(rows);
     return res.json(rows);
   } catch (err) {
+    console.log(err)
     return res.status(500).json(err)
   }
 });
